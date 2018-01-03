61a29ff27b0f1022e8ea50d1db036c41eae2a48e
Rob Whittaker <rob@purinkle.co.uk>
2018-01-03 17:02:36 +0000

    Add search functionality

--- src/App.js
+++ src/App.js
@@ -20,15 +20,20 @@ const list = [
   },
 ];
 
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       list,
+      searchTerm: '',
     };
 
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id) {
@@ -37,10 +42,20 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item =>
+        <form>
+          <input
+            onChange={this.onSearchChange}
+            type="text"
+          />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
